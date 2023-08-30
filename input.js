7ed2d881aae045e32a8040e1234b399d96720da4
Rita Tang <doggymint@gmail.com>
2023-08-29 21:10:26 -0400

    pass conn to input, allow keypress to send data to server directly

--- input.js
+++ input.js
@@ -1,12 +1,24 @@
+// Stores the active TCP connection object.
+let connection;
+
 const handleUserInput = (key) => {
   // ctrl+c to exit
   if (key === "\u0003") {
     process.exit();
+  } else if (key === "w") {
+    connection.write("Move: up");
+  } else if (key === "a") {
+    connection.write("Move: left");
+  } else if (key === "s") {
+    connection.write("Move: down");
+  } else if (key === "d") {
+    connection.write("Move: right");
   }
 };
 
 // setup interface to handle user input from stdin
-const setupInput = () => {
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
