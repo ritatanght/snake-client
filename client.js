2da8fd20cc43ce406b3869dd2adbe5b4ebd891c7
Rita Tang <doggymint@gmail.com>
2023-08-29 21:43:56 -0400

    move constants into constants.js

--- client.js
+++ client.js
@@ -1,18 +1,18 @@
 const net = require("net");
+const { IP, PORT, PLAYER } = require("./constants");
 
 // establishes a connection with the game server
 const connect = () => {
   const conn = net.createConnection({
-    host: "172.25.119.90", // IP address here,
-    port: 50541, // PORT number here,
+    host: IP, // IP address here,
+    port: PORT, // PORT number here,
   });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
   conn.on("connect", () => {
     console.log("Successfully connected to game server");
-    conn.write("Name: RTA");
- 
+    conn.write(`Name: ${PLAYER}`);
   });
 
   conn.on("data", (data) => {
---
+++ constants.js
@@ -0,0 +1,9 @@
+const IP = "172.25.119.90";
+const PORT = 50541;
+const PLAYER = "RTA";
+
+module.exports = {
+  IP,
+  PORT,
+  PLAYER,
+};
